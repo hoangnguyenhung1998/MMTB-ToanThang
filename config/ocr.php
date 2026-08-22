469fb77e714d86926945fe0bbdd32e4bbd7db068
hoangnguyenhung1998 <hunglatoi000vn@gmail.com>
2026-08-22 15:00:15 +0700

    Phase13: configure OCR sampling

--- config/ocr.php
+++ config/ocr.php
@@ -4,4 +4,5 @@ return [
     'worker_token' => env('OCR_WORKER_API_TOKEN'),
     'lease_seconds' => (int) env('OCR_JOB_LEASE_SECONDS', 300),
     'minimum_confidence' => (float) env('OCR_MINIMUM_CONFIDENCE', 0.80),
+    'review_sample_percent' => (int) env('OCR_REVIEW_SAMPLE_PERCENT', 3),
 ];
