c1ce2f224f4dbb77a52ee8f91e7314f0bed0e69c
hoangnguyenhung1998 <hunglatoi000vn@gmail.com>
2026-08-26 21:11:52 -0700

    Phase15: add automatic daily image exception center

--- app/Http/Requests/IndexDailyImageArchiveRequest.php
+++ app/Http/Requests/IndexDailyImageArchiveRequest.php
@@ -15,6 +15,7 @@ class IndexDailyImageArchiveRequest extends FormRequest
             'machine_id' => ['nullable', 'integer', 'exists:machines,id'],
             'command_center_id' => ['nullable', 'integer', 'exists:command_centers,id'],
             'completeness' => ['nullable', 'in:complete,incomplete'],
+            'exception_status' => ['nullable', 'in:EXCEPTIONS,AUTO_COMPLETE,NO_IMAGES,PENDING_REVIEW,DUPLICATE_TIME,MISSING_MARK,CTMS_PENDING,EXCESS_IMAGES'],
             'page' => ['nullable', 'integer', 'min:1'],
         ];
     }
